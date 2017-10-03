e16196b003d7a7061c56cf8a6e437163f4af991b
Jordan Marques <jordanmarques1994@gmail.com>
2017-10-03 09:45:50 +0200

    feat : add new value to balance table

--- src/app/balance-row-table/balance-row-table.component.ts
+++ src/app/balance-row-table/balance-row-table.component.ts
@@ -1,42 +1,55 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, TemplateRef} from '@angular/core';
 import {Month} from '../month';
 import {Row} from '../row';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
 
 @Component({
-  selector: 'app-balance-row-table',
-  templateUrl: './balance-row-table.component.html',
-  styleUrls: ['./balance-row-table.component.css']
+    selector: 'app-balance-row-table',
+    templateUrl: './balance-row-table.component.html',
+    styleUrls: ['./balance-row-table.component.css']
 })
 export class BalanceRowTableComponent implements OnChanges {
 
-  @Input() month: Month;
+    @Input() month: Month;
 
-  private balance: number;
-  private rows: Row[];
+    private balance: number;
+    private rows: Row[];
 
-  constructor() { }
+    public modalRef: BsModalRef;
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.loadTableValues(changes.month.currentValue);
-  }
+    constructor(private modalService: BsModalService) {
+    }
 
-  private debit(row: Row) {
-    this.updateBalance(row, (a: number, b: number) => a - b);
-  }
+    ngOnChanges(changes: SimpleChanges): void {
+        this.loadTableValues(changes.month.currentValue);
+    }
 
-  private credit(row: Row) {
-    this.updateBalance(row, (a: number, b: number) => a + b);
-  }
+    private debit(row: Row) {
+        this.updateBalance(row, (a: number, b: number) => a - b);
+    }
 
-  private updateBalance(row: Row, fn: (a: number, b: number) => number) {
-    this.balance = fn(this.balance, row.value);
-    row.isPayed = !row.isPayed;
-  }
+    private credit(row: Row) {
+        this.updateBalance(row, (a: number, b: number) => a + b);
+    }
 
-  private loadTableValues(month: Month) {
-    this.rows = month.rows;
-    this.balance = month.rows.filter(row => ! row.isPayed)
-                              .map(row => row.value)
-                              .reduce( (a, b) => a + b, 0);
-  }
+    private updateBalance(row: Row, fn: (a: number, b: number) => number) {
+        this.balance = fn(this.balance, row.value);
+        row.isPayed = !row.isPayed;
+    }
+
+    private loadTableValues(month: Month) {
+        this.rows = month.rows;
+        this.balance = month.rows.filter(row => !row.isPayed)
+            .map(row => row.value)
+            .reduce((a, b) => a + b, 0);
+    }
+
+    private addToList(newValue: number) {
+        this.rows.push(new Row(newValue, false));
+        this.modalRef.hide();
+    }
+
+    public openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
+    }
 }
