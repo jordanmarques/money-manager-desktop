09a06f386ebe024cf7c9bf3f0ba60dd051748824
Jordan Marques <jordanmarques1994@gmail.com>
2017-10-20 09:49:34 +0200

    remove test

--- src/app/balance-row-table/balance-row-table.component.spec.ts
+++ src/app/balance-row-table/balance-row-table.component.spec.ts
@@ -1,25 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BalanceRowTableComponent } from './balance-row-table.component';
 
 describe('BalanceRowTableComponent', () => {
-  let component: BalanceRowTableComponent;
-  let fixture: ComponentFixture<BalanceRowTableComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BalanceRowTableComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BalanceRowTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/monthly-page/monthly-page.component.spec.ts
+++ src/app/monthly-page/monthly-page.component.spec.ts
@@ -1,25 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MonthlyPageComponent } from './monthly-page.component';
 
 describe('MonthlyPageComponent', () => {
-  let component: MonthlyPageComponent;
-  let fixture: ComponentFixture<MonthlyPageComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MonthlyPageComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MonthlyPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
